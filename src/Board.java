ae659d46fccd3b7d20a3358b8eeb861c63667a69
bHodges <bhodges87@gmail.com>
2016-02-12 07:42:34 -0500

    "Reads first input from user (IB or IW), inits board, and prints R B or R W"

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Created by bHodges on 2/10/16.
  */
@@ -5,5 +7,62 @@
 //Board Class
 public class Board {
 
+    public static final int BORDER = -2;
+    public static final int EMPTY = 0;
+    int[][] board = new int[10][10];
+    boolean gameOver = false;
 
+    public Board(String color) {
+        initBoard(color);
+    }
+
+    private void initBoard(String color){
+        int black = 0;
+        int white = 0;
+        if (color.equals("Black")){
+            black = 1;
+            white = -1;
+            System.out.println("R B");
+        }else{
+            black = -1;
+            white = 1;
+            System.out.println("R W");
+        }
+
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                if (i == 0 || i == 9){
+                    board[i][j] = BORDER;
+                }else if (j == 0 || j == 9){
+                    board[i][j] = BORDER;
+                }else
+                    board[i][j] = EMPTY;
+            }
+        }
+
+        board[4][4] = white;
+        board[4][5] = black;
+        board[5][4] = black;
+        board[5][5] = white;
+    }
+
+    public void printBoard(){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j]>-1){
+                    System.out.print(" "+board[i][j]+" ");
+                }else
+                    System.out.print(board[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    public boolean isGameOver(){
+        if (gameOver){
+            return true;
+        }else
+            return false;
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 /**
  * Created by bHodges on 2/10/16.
  */
 
 //Main game class
 public class Game {
+    public static final int ME = 1;
+    public static final int OPPONENT = -1;
+    static int currentPlayer = 0;
+    static String myColor = null;
+    static Board board;
 
-   
+    public static void startGame() {
+        myColor = readInput();
+        if (myColor.equals("IB")){
+            currentPlayer = ME;
+        }else
+            currentPlayer = OPPONENT;
+
+        board = new Board(myColor);
+        board.printBoard();
+    }
+
+    public static String readInput(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input.replaceAll("\\s+","");
+    }
+
+    public void playGame(){
+        String move = null;
+        while(!board.isGameOver()){
+
+        }
+    }
 
     public static void main(String[] args){
-
+        startGame();
     }
+
 }
